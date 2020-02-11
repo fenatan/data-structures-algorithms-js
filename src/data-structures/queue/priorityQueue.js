b56b8c22ecca018c6a778b81cf5dbe7654f1089a
fenatan <felipenatan@novaandradina.org>
2020-02-11 00:52:16 -0400

    Adding PriorityQueue implementation and tests

---
+++ src/data-structures/queue/priorityQueue.js
@@ -0,0 +1,57 @@
+class PriorityQueue {
+    constructor() {
+        this._priorityQueue = [];
+    }
+
+    push(element) { //Should pass an Array [element, priority] - priority must be a INT
+        if (this.size() === 0) {
+            this._priorityQueue.push(element);
+        } else {
+            let flag = false;
+
+            for (let i = 0; i < this.size(); i++) {
+                if (element[1] > this._priorityQueue[i][1]) {
+                    this._priorityQueue.splice(i, 0, element);
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(!flag)
+                this._priorityQueue.push(element);
+        }
+    }
+
+    //remove the first element in the priorityQueue
+    pop(){
+        if(this.size() === 0)
+            return undefined;
+        return this._priorityQueue.shift()[0];
+    }
+
+    back(){
+        if(this.size() === 0) 
+            return undefined;
+        return this._priorityQueue[this.size() - 1][0];
+    }
+
+    front(){
+        if(this.size() === 0) 
+            return undefined;
+        return this._priorityQueue[0][0];
+    }
+
+    size() {
+        return this._priorityQueue.length;
+    }
+
+    isEmpty() {
+        return this.size() === 0;
+    }
+
+    clear(){
+        this._priorityQueue = [];
+    }
+}
+
+module.exports = PriorityQueue;
---
+++ src/data-structures/queue/tests/priorityQueue.js
@@ -0,0 +1,88 @@
+const assert = require('assert');
+const PriorityQueue = require('../priorityQueue');
+
+describe('Priority-Queue tests', function(){
+
+    it('Should create a Priority-Queue', () => {
+        const priorityQueue = new PriorityQueue();
+        assert.notEqual(priorityQueue, null);
+    });
+
+    it('Should add one element on the Priority-Queue', () => {
+        const priorityQueue = new PriorityQueue();
+
+        priorityQueue.push(['Spider Man', 5]);
+        priorityQueue.push(['Iron Man', 10]);
+        
+        assert.notDeepEqual(priorityQueue._priorityQueue, []);
+    });
+
+    it('Should return the size of Priority-Queue', () => {
+        const priorityQueue = new PriorityQueue();
+        assert.deepEqual(priorityQueue.size(), 0);
+
+        priorityQueue.push(['Spider Man', 5]);
+        priorityQueue.push(['Iron Man', 10]);
+        
+        assert.deepEqual(priorityQueue.size(), 2);
+    });
+
+    it('Should check if the Priority-Queue is empty', () => {
+        const priorityQueue = new PriorityQueue();
+        assert.ok(priorityQueue.isEmpty());
+
+        priorityQueue.push(['Spider Man', 5]);
+        priorityQueue.push(['Iron Man', 10]);
+        
+        assert.ok(!priorityQueue.isEmpty());
+    });
+
+    it('Should return the last element in Priority-Queue', () => {
+        const priorityQueue = new PriorityQueue();
+        assert.deepEqual(priorityQueue.back(), undefined);
+
+        priorityQueue.push(['Spider Man', 5]);
+        priorityQueue.push(['Hulk', 2]);
+        priorityQueue.push(['Iron Man', 10]);
+        
+        assert.deepEqual(priorityQueue.back(), 'Hulk');
+    });
+
+    it('Should return the first element in Priority-Queue', () => {
+        const priorityQueue = new PriorityQueue();
+        assert.deepEqual(priorityQueue.front(), undefined);
+
+        priorityQueue.push(['Spider Man', 5]);
+        priorityQueue.push(['Hulk', 2]);
+        priorityQueue.push(['Iron Man', 10]);
+        
+        assert.deepEqual(priorityQueue.front(), 'Iron Man');
+    });
+
+    it('Should remove the first element in Priority-Queue', () => {
+        const priorityQueue = new PriorityQueue();
+        assert.deepEqual(priorityQueue.pop(), undefined);
+
+        priorityQueue.push(['Spider Man', 5]);
+        priorityQueue.push(['Hulk', 2]);
+        priorityQueue.push(['Iron Man', 10]);
+        
+        assert.deepEqual(priorityQueue.pop(), 'Iron Man');
+    });
+
+    it('Should clear Priority-Queue', () => {
+        const priorityQueue = new PriorityQueue();
+        assert.deepEqual(priorityQueue.size(), 0);
+
+        priorityQueue.push(['Spider Man', 5]);
+        priorityQueue.push(['Hulk', 2]);
+        priorityQueue.pop()
+        priorityQueue.push(['Iron Man', 10]);
+        
+        assert.deepEqual(priorityQueue.front(), 'Iron Man');
+
+        priorityQueue.clear();
+
+        assert.deepEqual(priorityQueue.size(), 0);
+    });
+});
