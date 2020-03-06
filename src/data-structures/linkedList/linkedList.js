581bf04bd3436743ff636135dd59a567f00ace5f
Felipe Natan <fenatan@localhost.localdomain>
2020-03-05 22:13:43 -0400

    Adding Linked List documentation

--- src/data-structures/linkedList/linkedList.js
+++ src/data-structures/linkedList/linkedList.js
@@ -1,12 +1,28 @@
 const Node = require("./node");
-
+/**
+ * This class is a Linked List implementation
+ */
 class LinkedList {
+    /**
+    * Creates a Linked List data-structure
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    */
     constructor() {
         this._count = 0;
         this._head = null;
     }
 
-    // Add a element at the end of the list
+    /**
+    * Adds a element to the end of the linked list
+    * @param {*} element Element passed to insert
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.push(5); //inserts 5 to the end of the linked list
+    */
     push(element) {
         const node = new Node(element);
         if (this._head === null) this._head = node;
@@ -20,13 +36,37 @@ class LinkedList {
         this._count++;
     }
 
-    //should remove a element if exists
+    /**
+    * Removes a element if it exists
+    * @param {*} element Element passed to remove
+    * @returns {*|undefined} Returns the removed element or undefined
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.push(3);
+    * linkedList.push(9);
+    *
+    * linkedList.remove(3); //removes and returns number 3
+    */
     remove(element) {
         const index = this.indexOf(element);
         return this.removeAt(index);
     }
 
-    //remove a element from a specif index
+    /**
+    * Removes a element from a specific index if it exists
+    * @param {*} element Element passed to remove
+    * @returns {*|undefined} Returns the removed element or undefined
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.push(3);
+    * linkedList.push(9);
+    *
+    * linkedList.removeAt(1); //removes and returns number 9
+    */
     removeAt(index) {
         if (index >= 0 && index < this._count) {
             let currentNode = this.head();
@@ -43,7 +83,19 @@ class LinkedList {
         return undefined;
     }
 
-    //get element in the specific index
+    /**
+    * Returns a element from a specific index if it exists
+    * @param {*} index Index passed to return element
+    * @returns {*|undefined} Returns the element or undefined
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.push(3);
+    * linkedList.push(9);
+    *
+    * linkedList.getElementAt(1); //returns number 9
+    */
     getElementAt(index, flag = 0) {
         if (index >= 0 && index < this._count) {
             let currentNode = this._head;
@@ -56,7 +108,19 @@ class LinkedList {
         return undefined;
     }
 
-    // Adds a new element at a specific position
+    /**
+    * Inserts a element to the specific position of the linked list
+    * @param {*} element Element passed to insert
+    * @param {Number} index Index to insert
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.push(5);
+    * linkedList.push(7);
+    * 
+    * linkedList.insert(15, 1); // inserts 15 at index 1
+    */
     insert(element, index) {
         if (index >= 0 && index <= this._count) {
             const node = new Node(element);
@@ -75,7 +139,20 @@ class LinkedList {
         return false;
     }
 
-    //should return the index of a element in the linked list
+    /**
+    * Returns the index of a specific element if it exists
+    * @param {*} element Element passed to find the index
+    * @returns {Number} Returns the index or -1
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.push(3);
+    * linkedList.push(9);
+    *
+    * linkedList.indexOf(5); //returns -1
+    * linkedList.indexOf(3); //returns 0
+    */
     indexOf(element) {
         let currentNode = this._head;
         for (let i = 0; i < this._count; i++) {
@@ -86,19 +163,63 @@ class LinkedList {
         return -1;
     }
 
+    /**
+   * Returns the size of the Linked List
+   * @returns {Number} The number of elements in the Linked List
+   * @example
+   * const { LinkedList } = require('data-structures-algorithms-js');
+   * const linkedList = new LinkedList();
+   *
+   * linkedList.size(); // returns 0;
+   *
+   * linkedList.push(8);
+   *
+   * linkedList.size(); //returns 1;
+   */
     size() {
         return this._count;
     }
-
+    /**
+    * Returns the nodo on the head of the linked list
+    * @returns {Object} returns the head object with the element and next node 
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.push(23);
+    *
+    * linkedList.head(); //returns {element:23, next:null};
+    */
     head() {
         return this._head;
     }
-
+    /**
+    * Returns if the linked list is empty
+    * @returns {Boolean}
+    * @example
+    * const { LinkedList } = require('data-structures-algorithms-js');
+    * const linkedList = new LinkedList();
+    *
+    * linkedList.isEmpty(); // returns true;
+    *
+    * linkedList.push(23);
+    *
+    * linkedList.isEmpty(); //returns false;
+    */
     isEmpty() {
         return this.size() === 0;
     }
-
-    clear(){
+    /**
+   * Resets the Linked List
+   * @example
+   * const { LinkedList } = require('data-structures-algorithms-js');
+   * const linkedList = new LinkedList();
+   *
+   * linkedList.push(15);
+   *
+   * linkedList.clear(); // now linked list is empty
+   */
+    clear() {
         this._count = 0;
         this._head = null;
     }
